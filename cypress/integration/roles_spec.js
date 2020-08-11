e3cd6f449f12226b7c00d6d79e6263c840e49800
Juan Fagundez <juanfagundez96@gmail.com>
2020-08-11 17:45:47 -0300

    package.json added

--- cypress/integration/roles_spec.js
+++ cypress/integration/roles_spec.js
@@ -1,45 +1,45 @@
 /// <reference types="Cypress" />
 
-describe('Testing Roles', () => {
-    
-    beforeEach(() =>{
-        cy.visit('app/login')
+describe("Testing Roles", () => {
+  beforeEach("algo", () => {
+    cy.visit("app/login");
 
-        cy.get("input[name='username']")
-          .type('admin')
+    cy.get("input[name='username']").type("admin");
 
-        cy.get("input[name='password']")
-          .type('great tall cork')
+    cy.get("input[name='password']").type("great tall cork");
 
-        cy.get('button.Button')
-          .click()
-        
-        // otra manera de submitear el form
-        //cy.get('form').submit();
-        cy.debug()
-        //cy.visit('app/accounts/roles')
-    })
+    cy.get("button.Button").click();
+    cy.url({ timeout: 10000 })
+    .should("include", "incidents");
 
-    // // le pasas un label y lo selecciona
-    // it('Should go to Roles section', ()=>{
-    //         cy.get('button.Button.mr-2')
-    //           .click()
-    //         cy.get('div.DraggableColumnItem')
-    //         console.log(cy.get('div.DraggableColumnItem').lenght)
-    // })
+    // otra manera de submitear el form
+    // cy.get('form').submit();
+    //cy.debug()
 
-    // tiene que chequear que estén todos los atributos en la lista
-    it('Should go to Roles section', ()=>{
-        
-    })
+    cy.visit("/app/accounts/roles")
+      .url({ timeout: 10000 })
+      .should("include", "roles");
+  });
 
-    // // al menos 1 checkbox tiene que estar seleccionado
-    // it('Should go to Roles section', ()=>{
-        
-    // })
+  // le pasas un label y lo selecciona
+  it("Should go to Roles section", () => {
+    cy.get("button.Button.mr-2").click();
+    cy.get("div.DraggableColumnItem");
+    console.log(cy.get("div.DraggableColumnItem").length);
+  });
 
-    // // si agrego selecciono un checkbox, se tiene que agregar una columna en la vista
-    // it('Should go to Roles section', ()=>{
-        
-    // })
-})
+  // tiene que chequear que estén todos los atributos en la lista
+  // it('Should go to Roles section', ()=>{
+
+  // })
+
+  // // al menos 1 checkbox tiene que estar seleccionado
+  // it('Should go to Roles section', ()=>{
+
+  // })
+
+  // // si agrego selecciono un checkbox, se tiene que agregar una columna en la vista
+  // it('Should go to Roles section', ()=>{
+
+  // })
+});
